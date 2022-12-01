56626cb29ea49bce827e2e38022ba08dec581e3a
magnetenstad <tenstad.magne@gmail.com>
2022-12-01 16:11:39 +0100

    Add ranges to output filename, add example

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import pdf
+from pdf import extract
 
+extract("Advanced-Engineering-Mathematics", 
+  list(map(lambda x: (x[0]+26, x[1]+26), [
+    (203,237),
+    (473,491),
+    (495,498),
+    (510,517),
+    (522,533),
+    (540,556),
+    (558,574)
+  ]))
+)
--- pdf.py
+++ pdf.py
@@ -4,19 +4,19 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def extract(filename, ranges):
-	pdf_reader = PdfFileReader(filename)
+def extract(name, ranges):
+	pdf_reader = PdfFileReader(f"input/{name}.pdf")
 	pdf_writer = PdfFileWriter()
 	for r in ranges:
 		for n in range(r[0]-1, r[1]):
 			page = pdf_reader.getPage(n)
 			pdf_writer.addPage(page)
 
-	with Path(f"output/{filename}").open(mode="wb") as output_file:
+	with Path(f"output/{name}{ranges}.pdf").open(mode="wb") as output_file:
 		pdf_writer.write(output_file)
 
 def merge_folder(folder):
-	folder = "input/" + folder
+	folder = f"input/{folder}"
 	pdf_merger = PdfFileMerger(strict = False)
 	for filename in os.listdir(folder):
 		pdf = PdfFileReader(f"{folder}/{filename}")
