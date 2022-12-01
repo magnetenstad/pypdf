350c589a010a2cec92be855509f04d5a69fc07fd
magnetenstad <tenstad.magne@gmail.com>
2022-12-01 15:43:48 +0100

    Add pdf.py

---
+++ pdf.py
@@ -0,0 +1,26 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
+from pathlib import Path
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def extract(filename, ranges):
+	pdf_reader = PdfFileReader(filename)
+	pdf_writer = PdfFileWriter()
+	for r in ranges:
+		for n in range(r[0]-1, r[1]):
+			page = pdf_reader.getPage(n)
+			pdf_writer.addPage(page)
+
+	with Path(f"output/{filename}").open(mode="wb") as output_file:
+		pdf_writer.write(output_file)
+
+def merge_folder(folder):
+	folder = "input/" + folder
+	pdf_merger = PdfFileMerger(strict = False)
+	for filename in os.listdir(folder):
+		pdf = PdfFileReader(f"{folder}/{filename}")
+		pdf_merger.append(pdf)
+		
+	with Path(f"output/{folder}.pdf").open(mode="wb") as output_file:
+		pdf_merger.write(output_file)
